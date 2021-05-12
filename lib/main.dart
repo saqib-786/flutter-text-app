d18fd7e7d2a6cd04c2ade83073aa482681e182ef
saqib-786 <saqib.qumbrani@gmail.com>
2021-05-12 15:35:47 +0500

    flutter text app GIT

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: (
+          Text('Saqib Ali Qumbrani saqib.qumbrani@gmail.com')
+
+          
+
+        ),
+      ),
+      
+    );
+  }
+}
